90673baee913da975de78b0aa6dcf398108430fa
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-07-16 13:16:27 +0100

    update login

--- src/layouts/auth/Default.tsx
+++ src/layouts/auth/Default.tsx
@@ -34,16 +34,7 @@ function AuthIllustration(props: { children: ReactNode; illustrationBackground: 
 							width: 'fit-content',
 							marginTop: '40px'
 						}}>
-						<Flex
-							align='center'
-							ps={{ base: '25px', lg: '0px' }}
-							pt={{ lg: '0px', xl: '0px' }}
-							w='fit-content'>
-							<Icon as={FaChevronLeft} me='12px' h='13px' w='8px' color='secondaryGray.600' />
-							<Text ms='0px' fontSize='sm' color='secondaryGray.600'>
-								Back to Simmmple
-							</Text>
-						</Flex>
+				 
 					</a>
 				</Link>
 				{children}
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,25 +1,4 @@
-/* eslint-disable */
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2022 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
+ 
 
 import React from 'react';
 // Chakra imports
@@ -61,7 +40,7 @@ export default function SignIn() {
 	const [ show, setShow ] = React.useState(false);
 	const handleClick = () => setShow(!show);
 	return (
-		<DefaultAuthLayout illustrationBackground={'/img/auth/auth.png'}>
+		<DefaultAuthLayout illustrationBackground={'/img/auth/mail.jpg'}>
 			<Flex
 				maxW={{ base: '100%', md: 'max-content' }}
 				w='100%'
@@ -72,16 +51,9 @@ export default function SignIn() {
 				justifyContent='center'
 				mb={{ base: '30px', md: '60px' }}
 				px={{ base: '25px', md: '0px' }}
-				mt={{ base: '40px', md: '14vh' }}
+				mt={{ base: '14vh', md: '30vh' }}
 				flexDirection='column'>
-				<Box me='auto'>
-					<Heading color={textColor} fontSize='36px' mb='10px'>
-						Sign In
-					</Heading>
-					<Text mb='36px' ms='4px' color={textColorSecondary} fontWeight='400' fontSize='md'>
-						Enter your email and password to sign in!
-					</Text>
-				</Box>
+			 
 				<Flex
 					zIndex='2'
 					direction='column'
@@ -92,47 +64,24 @@ export default function SignIn() {
 					mx={{ base: 'auto', lg: 'unset' }}
 					me='auto'
 					mb={{ base: '20px', md: 'auto' }}>
-					<Button
-						fontSize='sm'
-						me='0px'
-						mb='26px'
-						py='15px'
-						h='50px'
-						borderRadius='16px'
-						bgColor={googleBg}
-						color={googleText}
-						fontWeight='500'
-						_hover={googleHover}
-						_active={googleActive}
-						_focus={googleActive}>
-						<Icon as={FcGoogle} w='20px' h='20px' me='10px' />
-						Sign in with Google
-					</Button>
-					<Flex align='center' mb='25px'>
-						<HSeparator />
-						<Text color='gray.400' mx='14px'>
-							or
-						</Text>
-						<HSeparator />
-					</Flex>
+		 
+				 
 					<FormControl>
 						<FormLabel display='flex' ms='4px' fontSize='sm' fontWeight='500' color={textColor} mb='8px'>
-							Email<Text color={brandStars}>*</Text>
+							Username<Text color={brandStars}>*</Text>
 						</FormLabel>
 						<Input
 							isRequired={true}
 							variant='auth'
 							fontSize='sm'
 							ms={{ base: '0px', md: '0px' }}
-							type='email'
-							placeholder='mail@simmmple.com'
+							type='text'
+							placeholder='aziz@mailing..'
 							mb='24px'
 							fontWeight='500'
 							size='lg'
 						/>
-						<FormLabel ms='4px' fontSize='sm' fontWeight='500' color={textColor} display='flex'>
-							Password<Text color={brandStars}>*</Text>
-						</FormLabel>
+					 
 						<InputGroup size='md'>
 							<Input
 								isRequired={true}
@@ -143,50 +92,19 @@ export default function SignIn() {
 								type={show ? 'text' : 'password'}
 								variant='auth'
 							/>
-							<InputRightElement display='flex' alignItems='center' mt='4px'>
-								<Icon
-									color={textColorSecondary}
-									_hover={{ cursor: 'pointer' }}
-									as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
-									onClick={handleClick}
-								/>
-							</InputRightElement>
+				 
 						</InputGroup>
 						<Flex justifyContent='space-between' align='center' mb='24px'>
-							<FormControl display='flex' alignItems='center'>
-								<Checkbox id='remember-login' colorScheme='brandScheme' me='10px' />
-								<FormLabel
-									htmlFor='remember-login'
-									mb='0'
-									fontWeight='normal'
-									color={textColor}
-									fontSize='sm'>
-									Keep me logged in
-								</FormLabel>
-							</FormControl>
-							<Link href='/auth/forgot-password'>
-								<a>
-									<Text color={textColorBrand} fontSize='sm' w='124px' fontWeight='500'>
-										Forgot password?
-									</Text>
-								</a>
-							</Link>
+					 	 
+					 
+						 
 						</Flex>
 						<Button fontSize='sm' variant='brand' fontWeight='500' w='100%' h='50' mb='24px'>
 							Sign In
 						</Button>
 					</FormControl>
 					<Flex flexDirection='column' justifyContent='center' alignItems='start' maxW='100%' mt='0px'>
-						<Text color={textColorDetails} fontWeight='400' fontSize='14px'>
-							Not registered yet?
-							<Link href='/auth/sign-up'>
-								<a>
-									<Text color={textColorBrand} as='span' ms='5px' fontWeight='500'>
-										Create an Account
-									</Text>
-								</a>
-							</Link>
-						</Text>
+		 
 					</Flex>
 				</Flex>
 			</Flex>
