e8f2c50dc990641adbe3989e0a632f6b4f5eb9ff
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-08-26 19:45:11 +0100

    Removing elements from print and optimize the code

--- src/pages/admin/print.tsx
+++ src/pages/admin/print.tsx
@@ -1,5 +1,5 @@
 
-import React, { useRef } from 'react'; 
+import React, { useRef, useState } from 'react'; 
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { Flex, Stack } from '@chakra-ui/react';
@@ -7,33 +7,34 @@ import AdminLayout from 'layouts/admin';
 import {useReactToPrint} from 'react-to-print'; 
 import { deleteCostumerFromListAction } from 'redux/costumerSlice';
 import { useDispatch, useSelector } from 'react-redux';
-export default function AdvancedDemo() {  
+export default function PrintComponent() {  
+    const singleRef = useRef(null) 
     const globalRef = useRef(null)
-
-      const handlePrintGlboal = useReactToPrint({
+    const [singleRefState,setSingleRefState] = useState(null)
+    const handlePrintGlboal = useReactToPrint({
         content: () => globalRef.current,
       });
-          const handleSinglePrint = useReactToPrint({
-        content: () => globalRef.current,
-        });
+     const handlePrintSingle = useReactToPrint({
+        content: () => singleRef.current,
+    })
     const costumersList = useSelector((state: any) => state.costumers)
     const dispatch = useDispatch()
     const header = (
         <img alt="Card" height={150}  src="https://strattonleocommunication.com/wp-content/uploads/2016/06/our-clients-banner.jpg" />
     );
  
-    const clientComponent = ({id,fullName,company,number,country,email,sector} : any) => {
-        return (
+    const ClientComponent = ({ id, fullName, company, number, country, email, sector }: any) => {
+    return (
                           <Stack margin={"auto"} my={4} width={"90%"}   >
             <Flex>
                    <div className="card flex justify-content-center">
                         <Card title={fullName} subTitle={company} footer={() => {
                             return (
-                                 <div className="flex flex-wrap justify-content-end gap-2">
+                                 <div ref={singleRef}  className="flex flex-wrap justify-content-end gap-2 noPrint">
                                     <Button label="Remove" onClick={() => {
                                         dispatch(deleteCostumerFromListAction({id:id}))
             }} icon="pi pi-trash" style={{backgroundColor:"red" , borderColor:"transparent" , marginRight:"10px"}} />
-            <Button onClick={(e) => {console.log(e)}} label="Print" icon="pi pi-paperclip" className="p-button-outlined p-button-secondary" />
+           
         </div>
                             )
             }} header={header} className="md:w-25rem">
@@ -54,11 +55,11 @@ export default function AdvancedDemo() {
     return (
         <AdminLayout>
             <Flex flexDir={"row"} margin={"auto"} justifyContent={"center"} >
-                <Button onClick={handlePrintGlboal}>Print {costumersList?.length} Card(s)  </Button>
+                <Button icon="pi pi-paperclip" style={{margin:15 , justifyContent:"space-between"}} label={"Print "+  costumersList?.length  +" Card(s)  "} onClick={handlePrintGlboal}> </Button>
             </Flex>
             <div ref={globalRef}>
                 {costumersList && costumersList.map((c:any) => {
-            return clientComponent(c)
+            return ClientComponent(c)
         })}
             </div>
       </AdminLayout>
